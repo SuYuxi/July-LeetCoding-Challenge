c2a1eb6530ff896f6d3e060031b45767b4d4e596
Yuxi <SuYuxi@users.noreply.github.com>
2020-07-08 18:39:47 +0900

    Update Day8_3Sum.cpp

--- Day8_3Sum.cpp
+++ Day8_3Sum.cpp
@@ -8,30 +8,30 @@ public:
         {
             if(i == 0 || nums[i] != nums[i - 1])
             {
-            int sum = nums[i];
-            int lo = i + 1;
-            int hi = n - 1;
-            while(lo < hi)
-            {
-                
-                if(nums[lo] + nums[hi] + sum == 0)
+                int sum = nums[i];
+                int lo = i + 1;
+                int hi = n - 1;
+                while(lo < hi)
                 {
-                    ans.emplace_back(vector<int>({sum, nums[lo], nums[hi]}));
-                    while(lo < hi && nums[lo] == nums[lo + 1]) lo++;
-                    while(lo < hi && nums[hi] == nums[hi - 1]) hi--;
-                    lo++;
-                    hi--;
-                }
-                else if(nums[lo] + nums[hi] + sum > 0)
-                {
-                    hi--;
-                }
-                else
-                {
-                    lo++;
+
+                    if(nums[lo] + nums[hi] + sum == 0)
+                    {
+                        ans.emplace_back(vector<int>({sum, nums[lo], nums[hi]}));
+                        while(lo < hi && nums[lo] == nums[lo + 1]) lo++;
+                        while(lo < hi && nums[hi] == nums[hi - 1]) hi--;
+                        lo++;
+                        hi--;
+                    }
+                    else if(nums[lo] + nums[hi] + sum > 0)
+                    {
+                        hi--;
+                    }
+                    else
+                    {
+                        lo++;
+                    }
                 }
             }
-                }
         }
         return ans;
     }
