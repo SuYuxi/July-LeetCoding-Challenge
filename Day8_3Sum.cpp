28372ab3c7a3cc60ebac519b349c2fa11dd8c69e
Yuxi <SuYuxi@users.noreply.github.com>
2020-07-08 18:37:05 +0900

    Create Day8_3Sum.cpp

---
+++ Day8_3Sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<vector<int>> ans;
+        for(int i = 0; i < n - 2; i++)
+        {
+            if(i == 0 || nums[i] != nums[i - 1])
+            {
+            int sum = nums[i];
+            int lo = i + 1;
+            int hi = n - 1;
+            while(lo < hi)
+            {
+                
+                if(nums[lo] + nums[hi] + sum == 0)
+                {
+                    ans.emplace_back(vector<int>({sum, nums[lo], nums[hi]}));
+                    while(lo < hi && nums[lo] == nums[lo + 1]) lo++;
+                    while(lo < hi && nums[hi] == nums[hi - 1]) hi--;
+                    lo++;
+                    hi--;
+                }
+                else if(nums[lo] + nums[hi] + sum > 0)
+                {
+                    hi--;
+                }
+                else
+                {
+                    lo++;
+                }
+            }
+                }
+        }
+        return ans;
+    }
+};
